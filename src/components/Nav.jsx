9890f6b045b27f6286a4fe077aa60b4778572bca
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-11-14 16:18:55 +0100

    updt

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,46 +1,52 @@
 import { useState } from "react";
 
 function NavBar() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-      // Function to toggle cart
-    const toggleCart = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  // Function to toggle cart
+  const toggleCart = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <nav>
-            <h1><button></button><button className="logo"></button> Auspices</h1>
-            <div className="desk-menu">
-                <ul>
-                    <li>About</li>
-                    <li>Services</li>
-                    <li>Project</li>
-                    <li>Review</li>
-                    <li>Blog</li>
-                </ul>
-            </div>
-            <button className="contact">contact</button>
-            <span className="toggle-menu"><ion-icon name="menu-outline" onClick={toggleCart} /></span>
-            {isMenuOpen && (
-        <div
-          className={`overlay ${isMenuOpen ? "show" : ""}`}
-          onClick={toggleCart}
-        ></div>
-      )}
-      <div className={`menu ${isMenuOpen ? "show" : ""}`}>
-        <span className="toggle-menu"> <ion-icon name="close-outline" onClick={toggleCart} /> </span>
+  return (
+    <nav>
+      <h1>
+        <button></button>
+        <button className="logo"></button> Auspices
+      </h1>
+      <div className="desk-menu">
         <ul>
-            <li>
-                About Us
-            </li>
-            <li>
-                Shop Now
-            </li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Project</li>
+          <li>Review</li>
+          <li>Blog</li>
         </ul>
       </div>
-        </nav>
-    )
+      <button className="contact">contact</button>
+      <span className="toggle-menu">
+        <ion-icon name="menu-outline" onClick={toggleCart} />
+      </span>
+
+      <div
+        className={`overlay ${isMenuOpen ? "show" : ""}`}
+        onClick={toggleCart}
+      ></div>
+      <div className={`menu ${isMenuOpen ? "show" : ""}`}>
+        <span className="toggle-menu">
+          <ion-icon name="close-outline" onClick={toggleCart} />
+        </span>
+        <ul>
+          <li>About</li>
+          <li>Services</li>
+          <li>Projects</li>
+          <li>Review</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
