3c4014dd549b333fc017b8d45dacb69f581ca121
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-11-14 20:08:30 +0100

    updt

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [linkActive, isLinkActive] = useState(null);
 
   // Function to toggle cart
   const toggleCart = () => {
@@ -37,12 +38,12 @@ function NavBar() {
           <ion-icon name="close-outline" onClick={toggleCart} />
         </span>
         <ul>
-          <li>About</li>
-          <li>Services</li>
-          <li>Projects</li>
-          <li>Review</li>
-          <li>Blog</li>
-          <li>Contact</li>
+          <li className={linkActive === 0 ? "active" : ""} onClick={() => isLinkActive(0)}>About</li>
+          <li className={linkActive === 1 ? "active" : ""} onClick={() => isLinkActive(1)}>Services</li>
+          <li className={linkActive === 2 ? "active" : ""} onClick={() => isLinkActive(2)}>Projects</li>
+          <li className={linkActive === 3 ? "active" : ""} onClick={() => isLinkActive(3)}>Review</li>
+          <li className={linkActive === 4 ? "active" : ""} onClick={() => isLinkActive(4)}>Blog</li>
+          <li className={linkActive === 5 ? "active" : ""} onClick={() => isLinkActive(5)}>Contact</li>
         </ul>
       </div>
     </nav>
